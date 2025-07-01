633b7d74507449994b4d2f86942ff2d089e0f049
Erik <33401464+ErikGrischuk@users.noreply.github.com>
2025-07-01 13:06:54 -0600

    Add files via upload

---
+++ start.ps1
@@ -0,0 +1,47 @@
+Start-Process C:\Users\Erik\Desktop\Server\rustds\RustDedicated.exe -ArgumentList `
+"-batchmode",`
+"+server.hostname `"[US] Tranquility:PvE`"",`
+"+server.ip 10.219.32.108",`
+"+server.maxplayers 10",`
+"+server.port 28080",`
+"+server.queryport 28015",`
+"+server.headerimage `"https://i.imgur.com/v6KVkUe.png`"",`
+"+server.url `"text.html`"",`
+"+server.levelurl `"file:///C:/Users/Erik/Desktop/Server/rustds/FancyOutpost.map`"",`
+"+server.logoimage `" `"",`
+"+server.tags `"biweekly,na,tut,vanilla`"",`
+"+server.description `"Hello`"",`
+"+rcon.ip 10.219.32.108",`
+"+rcon.port 28016",`
+"+rcon.password `"oxide23`"",`
+"+rcon.web 1",`
+"+server.secure `"True`"",`
+"+antihack.enabled `"True`"",`
+"+server.eac 1",`
+"+server.encryption 0",`
+"+server.tickrate 30",`
+"+fps.limit 256",`
+"+ai.move `"True`"",`
+"+server.entityrate  30",`
+"+server.level `"Procedural Map`"",`
+"+server.seed 2000",`
+"+server.salt 0",`
+"+server.worldsize 1000",`
+"+server.globalchat `"True`"",`
+"+chat.enabled `"True`"",`
+"+server.itemdespawn 300",`
+"+server.stability `"True`"",`
+"+heli.guns 1",`
+"+craft.instant `"False`"",`
+"+decay.scale 1",`
+"+server.radiation `"True`"",`
+"+eventschedulewipeoffset.event_hours_before_wipe 48",`
+"+bigwheelgame.spinfrequencyseconds 20",`
+"+hackablelockedcrate.requiredhackseconds 300",`
+"+snowmobile.allterrain `"True`"",`
+"+server.pve `"False`"",`
+"+clan.enabled `"True`"",`
+"+server.official `"True`"",`
+"+server.saveinterval 600",`
+"+antihack.terrain_protection 0",`
+"+server.identity `"Procedural Map`""
